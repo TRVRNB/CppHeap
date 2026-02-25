a22de4cdaec90b96caf26c594b6fb7e6a147f781
TRVRNB <travis.renberg@gmail.com>
2026-02-25 11:15:54 -0800

    1.2
    okay, two things.
    1) I added a Python script that will generate 100 numbers and output them to numbers.txt, and there is now a LOAD function where it will add all the numbers from this plain text file (or any other file the user enters) to the binary tree
    2) I accidentally uploaded my CppHashTable main.cpp earlier, this time I uploaded the correct file

---
+++ create_numbers.py
@@ -0,0 +1,19 @@
+"""
+This Python script will generate a file (numbers.txt) that can be used in main.cpp
+"""
+import sys, random
+
+sys.stdout = open("numbers.txt", "w") # print directly to numbers.txt
+data = ""
+used_numbers = []
+
+for i in range(100):
+	# generate 100 numbers, no repeats!
+	num = random.randint(1, 1000)
+	while num in used_numbers: # keep looking; there will always be an available number
+		num = random.randint(1, 1000)
+	data += str(num) # add this to the line
+	if i != 99: # add space if it's not the last number
+		data += " "
+
+print(data)
