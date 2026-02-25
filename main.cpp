8728d36f09163e45a8a8147c329590033ca3625e
TRVRNB <travis.renberg@gmail.com>
2026-02-25 11:58:04 -0800

    1.3
    Started working on the LOAD feature more, there is currently a compilation error but I WILL FIX IT!

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 // https://github.com/TRVRNB/CppHeap.git
 // this is a heap binary tree, a simple data structure that might end up being surprisingly complicated to implement
 #include <iostream>
-#include <fstream> // it wasn't listed in the assignment, but i think this is the best library for files, based on what i see online
+#include <fstream>
 #include <cmath> // for round()
 #include <cstring> // for numerous functions (useless comment, i know)
+#include <string>
 // apparently i might not actually need nodes for this
+// one major problem with this data structure i can think of off the top of my head, is that it doesn't handle collisions very well, especially near the bottom of the tree. 
+
 using namespace std;
+
 
 namespace heap_data {
   // i like to use a main namespace to share some variables between functions
-  const char version[10] = "1.2";
+  const char version[10] = "1.3";
   const unsigned int tree_size = 100; // this is extraneous and just exists to remind me
   unsigned int tree[101] = {0}; // this is unwrapped, and index 1 will be treated as index 0, since you can't double index 0 to get its children!
-  // i could also just add 1 to the index when doing math
+  // i could also just add 1 to the index when doing math, and it would have an absolutely negligible effect on both memory and performance, so why am i mentioning this?
 };
 using namespace heap_data;
 
 
 int add_to_next(int index, unsigned int num){
+  // REDO THIS FUNCTION! it has multiple errors now that i look it over, and doesn't even meet the requirements of the assignment
   // this is recursive, and will repeatedly go down the tree until it finds space
   // it won't look for the highest-available space, instead it will only look through 1 chain
   unsigned int parent_index = floor(index / 2);
@@ -47,6 +52,14 @@ int add_to_next(int index, unsigned int num){
 }
 
 
+bool is_digit2(char c){
+  // en.cppreference.com/w/cppp/string/byte/isdigit.html
+  // this is just a more convenient std::isdigit, since it works with regular chars
+  // and returns a bool instead of an int
+  return isdigit(static_cast<unsigned char>(c));
+}
+
+
 void add_to_tree(unsigned int num){
   // look for a valid spot, then add to the tree
   bool put_at_trunk = false;
@@ -72,21 +85,23 @@ int main(){
   while (true){
     // QUIT breaks the loop
     cout << "$ " << flush;
-    char input[81];
+    string input;
     cin >> input;
-    if (strcmp(input, "QUIT") == 0){ // quit
+    if (input == "QUIT"){ // quit
       cout << "Goodbye!" << endl;
       return 0;
-    } else if (strcmp(input, "HELP") == 0){ // help
+    } else if (input == "HELP"){ // help
       cout << "HELP: returns a list of commands (you knew that!)" << endl;
       cout << "QUIT: quits the program" << endl;
       cout << "LOAD: loads integers from a (plaintext) file" << endl;
-    } else if (strcmp(input, "LOAD") == 0){ // load
-      cout << "Enter the filename: " << flush;
-      char input[81];
+    } else if (input == "LOAD"){ // load
+      cout << "Enter the filename (max 80 chars): " << flush;
+      string input;
       cin >> input;
-      fstream file(input);
-      // at this point, there should be a variable called 'file' that stores the data
+      ifstream file(input);
+      char file_text[401]; // QUICK MATH says that 400 should be above the max length of such a file
+      file.getline(file_text); // file_text should now have file's data
+
     }
   }
 }
